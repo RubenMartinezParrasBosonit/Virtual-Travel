efb8cefecead43b929c1b78404cfb2f5eacdf890
Rubén Martínez Parras <ruben.mparras@bosonit.com>
2022-04-01 11:32:38 +0200

    Balanceo de carga con Eureka + gateway implementado

--- gateway/src/main/java/com/ruben/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/ruben/gateway/GatewayApplication.java
@@ -1,17 +1,15 @@
-package com.ruben.backweb;
+package com.ruben.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 @EnableEurekaClient
-public class BackwebApplication {
+public class GatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackwebApplication.class, args);
+		SpringApplication.run(GatewayApplication.class, args);
 	}
 
 }
